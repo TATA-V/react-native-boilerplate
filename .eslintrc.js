ee991a1c465878dd2a9062db2e854b645dce2e8d
tatahyeonv <xhrl523@naver.com>
2024-08-10 21:15:44 +0900

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,24 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  ignorePatterns: ['README.md', '.eslintrc.js', 'tsconfig.json'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'off',
     'max-len': 'off',
+    'object-curly-spacing': ['warn', 'always'],
+    'no-alert': 'off',
+    'curly': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/jsx-closing-bracket-location': [
+      'error',
+      'tag-aligned'
+    ],
+    'prettier/prettier': ['error',
+      {
+        printWidth: 150,
+        bracketSpacing: true,
+      }
+    ],
   },
 };
